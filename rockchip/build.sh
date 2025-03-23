5b15ef08eed896e27a6074fb11367dbbc1b3a66c
LimWolf <24842605+Lim2Wolf@users.noreply.github.com>
2025-03-23 10:42:18 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -40,6 +40,13 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-app-nikki"
+PACKAGES="$PACKAGES luci-app-adguardhome"
+PACKAGES="$PACKAGES luci-app-alist"
+PACKAGES="$PACKAGES luci-app-nekobox"
+PACKAGES="$PACKAGES luci-theme-design_new"
+PACKAGES="$PACKAGES luci-theme-partexp"
+
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
